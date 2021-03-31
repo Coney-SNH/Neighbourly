d8f5f4a778ee1dab95635b035df84cac607bf4d9
Pre-Bootcamp <77125588+BdoAni@users.noreply.github.com>
2021-03-30 20:42:30 -0700

    Add files via upload

--- Neighbourly/server/server.js
+++ Neighbourly/server/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const cookieParser = require('cookie-parser');
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 require('./config/mongoose.config');
 require('./routes/project.routes')(app);
+require('./routes/project.routes')(app);
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 
 app.listen(8000, () => console.log("Now listening on port 8000"));
