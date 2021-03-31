83722ae43e647f93ae702f0c92db4cd76c05c3db
Earl Lara <77307457+3elara@users.noreply.github.com>
2021-03-30 21:31:24 -0600

    3/30

--- Neighbourly/client/src/App.js
+++ Neighbourly/client/src/App.js
@@ -1,10 +1,20 @@
-import Create from './views/Create';
-import Main from './views/Main';
-import Detail from './views/Detail';
 import { Link } from '@reach/router';
 import { Router } from '@reach/router';
 import React, { useState } from 'react';
-import Edit from './views/Edit';
+import Main from './views/Main';
+//Create
+import Create from './views/User/UserCreate';
+import CreateTool from './views/Tool/ToolCreate';
+// import CreateReview from './views/Review/ReviewCreate';
+//Detail
+import Detail from './views/User/UserDetail';
+// import Detail from './views/Tool/ToolDetail';
+// import Detail from './views/ReviewDetail';
+//Edit
+import Edit from './views/User/UserEdit';
+// import Edit from './views/Tool/ToolEdit';
+// import Edit from './views/Review/ReviewEdit';
+
 import './App.css';
 
 function App(props) {
@@ -15,8 +25,19 @@ function App(props) {
         <Router>
         <Main path = "/" />
         <Create path = "/user/new" />
+        <CreateTool path = "/user/:id/new_tool" />
+        {/* <CreateReview path = "/user/new_review" /> */}
+
         <Edit path = "/user/:id/edit" />
+        {/* <EditTool path = "/user/:id/edit_review" />
+        <EditReview path = "/user/:id/edit_review" /> */}
+
         <Detail path = "/users/:id" />
+        {/* <DetailTool path = "/users/:id/detail_tool" />
+        <DetailReview path = "/users/:id/detail_review" /> */}
+
+
+
         </Router>
         </div>
     );
---
+++ Neighbourly/client/src/project.routes.js
@@ -0,0 +1,23 @@
+const ProjectController = require("../controllers/project.controller");
+
+module.exports = app => {
+    // Create
+    app.post("/api/user", ProjectController.createNewUser);
+    app.post("/api/user/:id/tool", ProjectController.createNewTool);
+    app.post("/api/user/:id/review", ProjectController.createNewReview);
+    // Read
+    app.get("/api/user", ProjectController.getAllUsers);
+    app.get("/api/tool/", ProjectController.getAllTools);
+    app.get("/api/user/:id", ProjectController.getOneUser);
+    app.get("/api/tool/:id", ProjectController.getOneTool);
+    app.get("/api/review/:id", ProjectController.getOneReview);
+    // Update
+    app.put("/api/user/:id", ProjectController.updateUser);
+    app.put("/api/tool/:id", ProjectController.updateTool);
+    app.put("/api/review/:id", ProjectController.updateReview);
+    // Delete
+    app.delete("/api/user/:id", ProjectController.deleteUser);
+    app.delete("/api/tool/:id", ProjectController.deleteTool);
+    app.delete("/api/review/:id", ProjectController.deleteReview);
+
+};
