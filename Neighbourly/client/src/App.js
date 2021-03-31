cb0f45ed3368721bde7b1909974df121617926ab
Pre-Bootcamp <77125588+BdoAni@users.noreply.github.com>
2021-03-30 23:11:55 -0700

    Add files via upload

--- Neighbourly/client/src/App.js
+++ Neighbourly/client/src/App.js
@@ -1,6 +1,10 @@
 import Create from './views/Create';
 import Main from './views/Main';
-import Detail from './views/Detail';
+import UserDetail from './views/UserDetail';
+import ToolDetail from './views/ToolDetail';
+import Connect from './views/Connect';
+import Payments from './views/Payments';
+import LoginRegister from './views/LoginRegister';
 import { Link } from '@reach/router';
 import { Router } from '@reach/router';
 import React, { useState } from 'react';
@@ -10,13 +14,18 @@ import './App.css';
 function App(props) {
     const [allusers, setAllUsers] = useState([]);
 
-    return ( <div className = "App">
-        <h1 > Neighborly </h1>
-        <Router>
-        <Main path = "/" />
+    return ( < div className = "App">
+        <h1> Neighborly </h1> 
+        <Link to = { `/` } > back to home </Link> <br/>
+        <Router >
+        <LoginRegister path = "/"/>
+        <Main path = "/homepage"/>
         <Create path = "/user/new" />
         <Edit path = "/user/:id/edit" />
-        <Detail path = "/users/:id" />
+        <UserDetail path = "/users/:id" />
+        <ToolDetail path = "/tool/:id"/>
+        <Connect path = "/user/:id/connect"/>
+        <Payments path = "/user/:id/checkout"/>
         </Router>
         </div>
     );
