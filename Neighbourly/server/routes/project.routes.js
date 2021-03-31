bf735df0c052b0cf9ded69169d28b10f3ef2df14
Pre-Bootcamp <77125588+BdoAni@users.noreply.github.com>
2021-03-30 23:17:54 -0700

    Add files via upload

--- Neighbourly/server/routes/project.routes.js
+++ Neighbourly/server/routes/project.routes.js
@@ -3,6 +3,7 @@ const ProjectController = require("../controllers/project.controller");
 module.exports = app => {
     // Creating a new author
     app.post("/api/user", ProjectController.createNewUser);
+    app.post("/api/user/login", ProjectController.loginUser);
     // R
     app.get("/api/user", ProjectController.getAllUsers);
     app.get("/api/user/:id", ProjectController.getOneUser);
