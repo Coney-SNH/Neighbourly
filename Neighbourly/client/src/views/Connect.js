e35e1c1ba0975b80bf025ee1f53d415e8f261ff3
Pre-Bootcamp <77125588+BdoAni@users.noreply.github.com>
2021-03-31 20:36:28 -0700

    Map File

--- Neighbourly/client/src/views/Connect.js
+++ Neighbourly/client/src/views/Connect.js
@@ -1,27 +1,12 @@
-<<<<<<< HEAD
 import React from 'react'
 
 const Connect = () => {
-    return (
-        <div>
-            <h1>You can Connect now! Send a Message</h1>
-        </div>
+    return ( <
+        div >
+        <
+        h1 > You can Connect now!Send a Message < /h1> <
+        /div>
     )
 }
 
 export default Connect
-
-=======
-import React from 'react'
-
-const Connect = () => {
-    return (
-        <div>
-            <h1>You can Connect now! Send a Message</h1>
-        </div>
-    )
-}
-
-export default Connect
-
->>>>>>> Ani_Bdoian_Main_Project
--- Neighbourly/client/src/views/Payments.js
+++ Neighbourly/client/src/views/Payments.js
@@ -1,24 +1,11 @@
-<<<<<<< HEAD
 import React from 'react'
 
 const Payments = () => {
     return (
          <div >
-        <h1 > Your payment methods! < /h1> 
+        <h1 > Your payment methods! </h1> 
         </div>
     )
 }
 
-=======
-import React from 'react'
-
-const Payments = () => {
-    return (
-         <div >
-        <h1 > Your payment methods! < /h1> 
-        </div>
-    )
-}
-
->>>>>>> Ani_Bdoian_Main_Project
 export default Payments;
